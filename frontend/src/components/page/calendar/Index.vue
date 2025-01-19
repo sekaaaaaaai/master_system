ca231b29ff2fa4163173cd3225634ad4c0c08160
Sekaaaaaaai <iznore147@gmail.com>
2025-01-19 22:21:39 +0900

    一覧画面から社員を選択できるように変更

--- frontend/src/components/page/calendar/Index.vue
+++ frontend/src/components/page/calendar/Index.vue
@@ -13,6 +13,28 @@
 
     <v-spacer></v-spacer>
 
+    <!-- 行が選択されている場合のみ削除ボタンを表示 -->
+    <v-btn
+      color="red-darken-1"
+      variant="flat"
+      class="me-3"
+      @click="handleDeleteEmployee"
+      v-if="selected && selected.length > 0"
+    >
+      削除
+    </v-btn>
+
+    <!-- 行が選択されている場合のみ編集ボタンを表示 -->
+    <v-btn
+      color="green-darken-1"
+      variant="flat"
+      class="me-3"
+      @click="handleEditEmployee"
+      v-if="selected && selected.length > 0"
+    >
+      編集
+    </v-btn>
+
     <v-btn
       color="primary"
       variant="flat"
@@ -32,31 +54,16 @@
     @input="onInputDebounced"
     />
 
-    <v-table>
-      <thead>
-        <tr>
-          <th class="text-left">
-            <b>ID</b>
-          </th>
-          <th class="text-left">
-            <b>氏名</b>
-          </th>
-          <th class="text-left">
-            <b>Email</b>
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr
-          v-for="item in employees"
-          :key="item.id"
-        >
-          <td>{{ item.id }}</td>
-          <td>{{ item.last_name }} {{ item.first_name }}</td>
-          <td>{{ item.email }}</td>
-        </tr>
-      </tbody>
-    </v-table>
+    <v-data-table 
+      :items="visibleEmployees"
+      :headers="headers"
+      item-value="id"
+      v-model="selected"
+      select-strategy="single"
+      show-select
+      @update:model-value="handleCheckStateChanged"
+    ></v-data-table>
+
 
   </v-container>
 
@@ -70,11 +77,77 @@ import router from '../../../plugins/router';
 
 axios.defaults.baseURL = 'http://192.168.128.2:53000';
 
+type EmployeeData = {
+    id: string;
+    first_name: string;
+    middle_name: string | null;
+    last_name: string;
+    first_name_kana: string;
+    middle_name_kana: string | null;
+    last_name_kana: string;
+    email: string;
+    joined_at: string;
+    retired_at: string | null;
+    created_at: string;
+    updated_at: string;
+};
+
+type VisibleEmployeeData = {
+    id: string;
+    name: string;
+    name_kana: string;
+    email: string;
+    joined_at: string;
+    retired_at: string | null;
+};
+
+function formatEmployeeData(input: EmployeeData): VisibleEmployeeData {
+    const name = input.middle_name
+        ? `${input.last_name} ${input.first_name} ${input.middle_name}`
+        : `${input.last_name} ${input.first_name}`;
+
+    const name_kana = input.middle_name_kana
+        ? `${input.last_name_kana} ${input.first_name_kana} ${input.middle_name_kana}`
+        : `${input.last_name_kana} ${input.first_name_kana}`;
+
+    return {
+        id: input.id,
+        name,
+        name_kana,
+        email: input.email,
+        joined_at: input.joined_at,
+        retired_at: input.retired_at,
+    };
+}
+
+function formatEmployeeDataArray(inputs: EmployeeData[]): VisibleEmployeeData[] {
+    return inputs.map(formatEmployeeData);
+}
+
+const headers = ref([
+  { title: "ID", key: "id" },
+  { title: "名前", key: "name" },
+  { title: "名前（かな）", key: "name_kana" },
+  { title: "メールアドレス", key: "email" },
+  { title: "入社日", key: "joined_at" },
+  { title: "退職日", key: "retired_at" },
+]);
+
 const employees = ref();
+const visibleEmployees = ref();
 const searchText = ref("");
+const selected = ref();
 
 const handleNewEmployee = () => { // 社員追加ページに移動
-  router.push('calendar/new');
+  router.push('/calendar/new');
+};
+
+const handleEditEmployee = () => { // 社員追加ページに移動
+  router.push('/calendar/edit');
+};
+
+const handleDeleteEmployee = () => { // 社員追加ページに移動
+  confirm('削除します。よろしいですか？')
 };
 
 const getEmployees = (q: string | null = null)=>{
@@ -84,6 +157,7 @@ const getEmployees = (q: string | null = null)=>{
     },
   }).then((res) =>{
     employees.value = res.data; // 結果をリアクティブな変数にセット
+    visibleEmployees.value = formatEmployeeDataArray(employees.value); // 表示用にデータを整形
     console.log(employees.value); // レスポンスデータをコンソールに出力
   }).catch((error) => {
     console.error("エラーが発生しました:", error);
@@ -96,6 +170,10 @@ const onInputDebounced = _.debounce(() => {
   getEmployees(text);
   // サーバーリクエストや他の処理を記述
 }, 300); // 300msの遅延
+
+const handleCheckStateChanged = () => {
+  console.log(selected.value)
+}
 
 //DOM読み込み後に展開する
 onMounted(async()=>{
